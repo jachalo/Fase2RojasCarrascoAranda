57af5ce980a251a723ec324689eb61eb9c8b289a
gonzalo rojas otani <go.rojaso@alumnos.duoc.cl>
2020-10-22 16:47:12 -0300

    animespedia: Creando Modelos

--- animespedia/animes/models.py
+++ animespedia/animes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Autor(models.Model):
+	nombre = models.CharField(max_length=100)
+	apellido = models.CharField(max_length=100)
+	nacimiento = models.DateField()
+
+class AnimeGen(models.Model):
+	"""Model representing an author."""
+	name = models.CharField(max_length=100)
+	summary = models.TextField(max_length=1000)
+
+	class Meta:
+		ordering = ['name']
+
+	#def get_absolute_url(self):
+		#return reverse('genre-detail', args=[str(self.id)]) #entrega la url del recurso
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return self.name
+
+class Anime(models.Model):
+	nombre=models.CharField(max_length=100)
+	descripcion = models.TextField(max_length=1000)
+	autor=models.ForeignKey(Autor,on_delete= models.CASCADE)
+	genero=models.ForeignKey(AnimeGen,on_delete= models.CASCADE)
+
