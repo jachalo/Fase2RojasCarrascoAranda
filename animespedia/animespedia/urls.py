18cbffd8c3fc8bfa853f3416d206fbabc0ba5ff4
gonzalo rojas otani <go.rojaso@alumnos.duoc.cl>
2020-10-23 10:46:49 -0300

    animespedia:Implementando pagina de inicio

--- animespedia/animespedia/urls.py
+++ animespedia/animespedia/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
+from animes.views import inicio
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('animes/', include('animes.urls')), 
+    path ('animes/', include('animes.urls')),
+    path ("", inicio, name="inicio"),
+    
 ]
